62d8251dc692701303731708859899f5dd82b095
Lucas Wiman <lucaswiman@counsyl.com>
2016-09-15 10:48:29 -0700

    Add base trove classifiers for python 2/3.

--- setup.py
+++ setup.py
@@ -69,7 +69,9 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
     ],
     packages=find_packages(exclude=[
