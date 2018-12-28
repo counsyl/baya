98daec5e6817b70aeb3c8f3761e948e258dc2102
Hunter Richards <hunter@counsyl.com>
2018-12-28 11:45:10 -0600

    Re-allow higher django-auth-ldap versions

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         "Programming Language :: Python :: 3.5",
     ],
     install_requires=[
-        'django-auth-ldap>=1.2.8<1.5',
+        'django-auth-ldap>=1.2.8',
         'six>=1.3'
     ],
     packages=find_packages(exclude=[
